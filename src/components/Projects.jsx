303e8ee6113e45a98511ada40e44342b0fa57269
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-04-27 16:50:45 +0100

    Made projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,19 +17,185 @@ import Project15 from "../assets/images/ProjectImg/Project15.png";
 import Project16 from "../assets/images/ProjectImg/Project16.png";
 
 
-// const projects = [
-//     {
-//         name: String,
-//         link: String,
-//         img: String,
-//         category: String,
-//         responsive: Boolean,
-//     },
-// ]
+const projects = [
+    {
+        name: "Fylo Landing page",
+        link: "https://jolomitee.github.io/SH_Capstone_Project_1/",
+        img: Project1,
+        category: "frontend mentor, landing page, bootstrap",
+        responsive: true,
+    },
+    {
+        name: "Zuri Landing Page",
+        link: "https://jolomitee.github.io/Zuri-Final-Task/",
+        img: Project2,
+        category: "zuri, task, landing page",
+        responsive: true,
+    },
+    {
+        name: "Sign Up Page",
+        link: "https://jolomitee.github.io/Zuri-Projects/",
+        img: Project3,
+        category: "zuri, task",
+        responsive: true,
+    },
+    {
+        name: "Museum of Candy",
+        link: "https://jolomitee.github.io/Proj_Collab_1_Divine_and_Taiwo/",
+        img: Project4,
+        category: "frontend mentor, challenge",
+        responsive: true,
+    },
+    {
+        name: "Mock Admin Dashboard",
+        link: "https://jolomitee.github.io/Admin_Dashboad_1/",
+        img: Project5,
+        category: "mock design",
+        responsive: true,
+    },
+    {
+        name: "Bookmark Landing Page",
+        link: "https://bookmark-meec.onrender.com/https://jolomitee.github.io/Admin_Dashboad_1/",
+        img: Project6,
+        category: "landing page, tailwindcss",
+        responsive: true,
+    },
+    {
+        name: "Manage Landing Page",
+        link: "https://jolomitee.github.io/first-tailwind-project/",
+        img: Project7,
+        category: "landing page, tailwindcss",
+        responsive: true,
+    },
+    {
+        name: "SaveSpace Landing Page",
+        link: "https://jolomitee.github.io/SaveSpace/",
+        img: Project8,
+        category: "landing page, tailwindcss",
+        responsive: true,
+    },
+    {
+        name: "CatchUp Landing Page",
+        link: "https://catchup-omega.vercel.app/",
+        img: Project9,
+        category: "landing page, tailwindcss",
+        responsive: true,
+    },
+    {
+        name: "CatchUp Landing Page",
+        link: "",
+        img: Project10,
+        category: "react, weather-api, geolocation-api",
+        responsive: true,
+    },
+    {
+        name: "Text Editor",
+        link: "https://jolomitee.github.io/JS-Text-Editor/",
+        img: Project11,
+        category: "javascript",
+        responsive: true,
+    },
+    {
+        name: "Photo Album",
+        link: "https://mui-sage.vercel.app/",
+        img: Project12,
+        category: "react, material-ui ",
+        responsive: true,
+    },
+    {
+        name: "Advent Of CSS",
+        link: "https://mui-sage.vercel.app/",
+        img: Project13,
+        category: "advent-of-css, 24-challenges, CSS ",
+        responsive: true,
+    },
+    {
+        name: "Subasta E-commerce",
+        link: "https://commercio.onrender.com",
+        img: Project14,
+        category: "frontend design, bootstrap, e-commerce site",
+        responsive: true,
+    },
+    {
+        name: "HooBank Landing Page",
+        link: "https://hoobank-dkt9.onrender.com",
+        img: Project15,
+        category: "react, tailwindcss, landing page",
+        responsive: true,
+    },
+    {
+        name: "Portfolio Design",
+        link: "",
+        img: Project16,
+        category: "react, tailwindcss, portfolio page",
+        responsive: true,
+    },
+    // {
+    //     name: String,
+    //     link: String,
+    //     img: String,
+    //     category: String,
+    //     responsive: Boolean,
+    // },
+]
+function splitStr(str, separator){
+    const categoryArray = str.split(separator)
+    return categoryArray.map((item, index) => (
+        <span key={index} className="tag border border-solid border-white-2 px-3 rounded-xl">{item}</span>
+    ))
+}
+
+let comma = ','
+const projectList = projects.reverse()
 
 const Projects = () => {
   return (
-    <article>Projects</article>
+    <article className='portfolio'>
+        <header>
+            <h2 className="h2 article-title mb-6">My Projects</h2>
+        </header>
+        <article className="text-white-1 mb-6">
+            Projects are uploaded every now and then
+        </article>
+
+        <section className="projects">
+            <ul className="project-list flex justify-center flex-wrap gap-8 mb-3">
+                {
+                    projectList.map((project, index) => (
+
+                        <li key={index} class="project-item block">
+
+                            <a className='w-full' target="_blank" href={project.link}>
+
+                                <figure class="project-img relative w-full h-[200px] rounded-2xl overflow-hidden mb-4 before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-opacity-0 z-[1] transition-1">
+
+                                    <div class="project-item-icon-box scale-.75 bg-jet text-orange-yellow-crayola absolute top-[35%] left-[40%] text-[20px] p-5 rounded-xl opacity-0 z-[1] transition-1">
+                                        <ion-icon name="eye-outline"></ion-icon>
+                                    </div>
+
+                                    <img src={project.img} alt={project.name} loading="lazy" className='w-full h-full object-cover transition-1'/>
+
+                                </figure>
+
+                                <h3 class="project-title ml-3 mb-2 text-white-1 text-fs-4 font-fw-400 capitalize leading-[1.3]">
+                                    {project.name}
+                                </h3>
+
+                                <div className="tags ml-3 flex flex-wrap gap-2 text-vegas-gold text-fs-7">
+                                    {
+                                        splitStr(project.category, comma)
+                                    }
+                                </div>
+                            </a>
+                        </li>
+                    ))
+                }
+
+
+            </ul>
+        </section>
+
+    </article>
   )
 }
 
