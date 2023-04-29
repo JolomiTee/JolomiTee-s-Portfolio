4da874b036c913a34ebefc9555728c99ee351edf
Taiwo Jolomi <taiwojolomi15@gmail.com>
2023-04-29 13:26:51 +0100

    Deleted ,mainComponent file to test shared layout routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import './App.css'
-import MainContent from './components/MainContent'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import About from './components/About'
+import Resume from './components/Resume'
+import Projects from './components/Projects'
+import Error from './components/Error'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App mb-20">
         <Sidebar />
         <Navbar />
-        <MainContent />
+        <BrowserRouter>
+            <Routes>
+
+                <Route path='/' element={<About />} />
+                <Route path='resume' element={<Resume />} />
+                <Route path='projects' element={<Projects />} />
+                <Route path='*' element={<Error />} />
+
+            </Routes>
+
+        </BrowserRouter>
     </div>
   )
 }
